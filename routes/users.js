c4cc160648d6e510c9e80ed839b1ddd665579f52
Trendafil <tdtrendafilov@gmail.com>
2018-04-26 02:34:35 +0300

    backend: user registration

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,37 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const sha1 = require('sha1');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+//Register new user
+router.post('/register', function (req, res, next) {
+    var usersCollection = req.db.get('users');
+    var newUser = req.body;
+    newUser.email = '';
+    newUser.playlists = [];
+    newUser.uploadedVideos = [];
+    newUser.history = [];
+
+    if (newUser.username.trim() == '') {
+        res.status(412);
+        res.json({
+            error: "please enter username"
+        });
+        return;
+    }
+
+    if (String(newUser.password).trim().length < 8) {
+        res.status(412);
+        res.json({
+            error: "the password must have at least 8 characters"
+        });
+        return;
+    }
+
+    newUser.password = sha1(newUser.password);
+
+    usersCollection.insert(newUser, function (err, data) {
+        res.json({id: data._id});
+    });
 });
 
 module.exports = router;
