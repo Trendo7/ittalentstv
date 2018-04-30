e9a28861007eb94a34c4831a12b72d43d908641c
Trendafil <tdtrendafilov@gmail.com>
2018-05-01 02:55:33 +0300

    small cnages

--- public/assets/js/videos/videosController.js
+++ public/assets/js/videos/videosController.js
@@ -1,11 +1,13 @@
 app.controller('VideosController', function ($scope, $window, $location, VideosService) {
     $scope.videos = [];
 
-    VideosService.getVideos().then(function (videos) {
-        $scope.$apply(function () {
-            $scope.videos = videos;
-        });
-    });
+    VideosService.getVideos()
+        .then(function (videos) {
+            $scope.$apply(function () {
+                $scope.videos = videos;
+            });
+        })
+        .catch(err => console.log(err));
 
 
     $scope.openLink = function (video) {
@@ -32,22 +34,27 @@ app.controller('CurrentVideoController', function ($scope, $location, VideosServ
 
 
     //gets all videos in the right sidebar
-    VideosService.getVideos().then(function (videos) {
-        $scope.$apply(function () {
-            $scope.videos = videos;
-        });
-    });
+    VideosService.getVideos()
+        .then(function (videos) {
+            $scope.$apply(function () {
+                $scope.videos = videos;
+            });
+        })
+        .catch(err => console.log(err));
 
 
     //update video rate (like/dislike) of watched video
     $scope.updateVideoRate = function (vote) {
         VideosService.updateVideoRate($scope.videoID, {vote: vote})
-            .then(function (response){
+            .then(function (response) {
                 $scope.$apply(function () {
                     $scope.currentVideo.likedByUserIDs = response.data.likedByUserIDs;
                     $scope.currentVideo.dislikedByUserIDs = response.data.dislikedByUserIDs;
                 });
-            });
+            })
+            //--->>> if error is 401 must show option for login <<<---
+            //--->>> if error is 403 must alert that this video is uploaded by the same user <<<---
+            .catch(err => console.log(err));
     };
 
 
