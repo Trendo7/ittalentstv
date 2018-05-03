041ee3afc1a7d488ce6c4841f8a0cab130da1500
Christian Petrov <c2petrov@gmail.com>
2018-05-04 01:19:10 +0300

    user profile fixes//upload section fixes/changes// other stuff

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -11,7 +11,7 @@ app.config(function ($routeProvider) {
             controller: 'SigninController'
         })
         .when('/upload', {
-            templateUrl: 'assets/js/videos/upload.htm',
+            templateUrl: 'assets/js/upload/upload.htm',
             controller: 'UploadController'
         })
         .when('/account', {
